d26db19caf0359c4b26988a1061575c6a5b7fe3f
yunikoro <542031180@qq.com>
2017-06-26 22:51:56 +0800

    nosence start

---
+++ nosence-base/src/main/java/com/zhheng/nosencebase/NosenceBaseApplication.java
@@ -0,0 +1,12 @@
+package com.zhheng.nosencebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosenceBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NosenceBaseApplication.class, args);
+	}
+}
